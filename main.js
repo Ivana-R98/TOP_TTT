0e09f1e1a428dbc752c4a4c289d5bcaec24677ce
Ivana-R98 <ivannarosic98@gmail.com>
2023-03-21 16:21:16 +0100

    created a function that checks if someone won

--- main.js
+++ main.js
@@ -1,49 +1,38 @@
 // the Gameboard object
 const Gameboard = (() => {
-    const board = [];
-    for(let i = 0; i < 9; i++) {
-        board.push("");
-    }
-
+    const board = Array(9).fill("");
     const divs = document.querySelectorAll(".item");
     let move = 0;
-    let id = 0;
-Array.from(divs).forEach(div => {
-        div.addEventListener("click", () => { 
-            div.style.backgroundColor = "white";
-            id = parseInt(div.id);
-            
-            if(move === 0 || move % 2 === 0) {
-                div.innerText = player1.mark;
-                let val = div.setAttribute("value","X");
-                board[move] = div;
-                console.log(div.id, board[move], div.getAttribute("value"), val, board[move].id);
-            }
-            else {
-                div.innerText = player2.mark;
-                div.setAttribute("value","O");
-                let val = div.getAttribute("value");
-                board[move] = div;
-                console.log(div.id, board[move], div.getAttribute("value"), val, board); 
-            } 
-            console.log(div, parseInt(div.id), div.innerText)
-            if (move < 9) {
-                console.log("check winner", id);
-                game.test()
-                if(move === 8) {
-                    console.log(game.winner);
+    let id;
+    Array.from(divs).forEach(div => {
+        div.addEventListener("click", (e) => { 
+            if(div.innerText === "") {
+                div.style.backgroundColor = "white";
+                id = parseInt(div.id);
+                if(move === 0 || move % 2 === 0) {
+                    div.innerText = player1.mark;
+                    board[e.target.id] = player1.mark;
                 }
+                else if(move % 2 !== 0) {
+                    div.innerText = player2.mark;
+                    board[e.target.id] = player2.mark;
+                } 
+                console.log(board);
+                // console.log(id, div.innerText);
+                if (move < 9) {
+                    if(move > 3) {
+                        //check if someone won
+                        Game.checkWinner()
+                        console.log(Game.winner)
+                    }
+                    if(move === 8)
+                    console.log("Tie!");
+                }
+                move++;
             }
-            move++;
-            
         })
     });
-
-
-
-
-
-    return { board, divs, move, id };
+    return { board, divs };
 })();
 
 
@@ -55,47 +44,32 @@ const player1 = Player("Player 1", "X");
 const player2 = Player("Player 2", "O");
 
 // game control
-const game = (() => {
+const Game = (() => {
+    let winner = false;
     const winningArr = [[0, 1, 2], [0, 3, 6], [0, 4, 8],
     [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]];
-    const test = () => {
-        if (Gameboard.move === 5) {
-            console.log("bla bla works")
-            return "kek";
-        }
+
+    const checkWinner = () => {
+        winningArr.forEach(item => {
+            if(Gameboard.board[item[0]] === player1.mark 
+                && Gameboard.board[item[1]] === player1.mark 
+                && Gameboard.board[item[2]] === player1.mark) {
+                console.log(`Winner ${player1.name}`);
+                winner = true; return winner;
+            }
+            else if(Gameboard.board[item[0]] === player2.mark 
+                && Gameboard.board[item[1]] === player2.mark 
+                && Gameboard.board[item[2]] === player2.mark) {
+                console.log(`Winner ${player2.name}`);
+                winner = true; return winner;
+            }
+        })
+        return { winner }
     }
-    let winner = false;
-    return { winner, winningArr, test }
+    return { winningArr, checkWinner, winner}
 })();
 
 //display
-const display = (() => {
+// const display = (() => {
 
-})();
-
-
-
-
-// console.log(player1.name, player2.mark);
-
-
-/* 
-    if(move === 0 && move % 2 === 0) {
-        divs.innerText = "x";
-        gameboard.push("x");
-        divs.dataset.index = i;
-        divs.style.backgroundColor = "white";
-        click++;
-        move++;
-        console.log(divs.dataset.index, gameboard[i], click, "x");
-    }
-    else if(move % 2 != 0) {
-        divs.innerText = "o";
-        gameboard.push("o");
-        divs.dataset.index = i;
-        divs.style.backgroundColor = "white";
-        click++;
-        move++;
-        console.log(divs.dataset.index, gameboard[i], click, "o"); 
-    } 
-*/
+// })();
